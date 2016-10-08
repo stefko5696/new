9945036c7805a7e11a984e3546222b04ece5418e
Eric Tran <tung.tran@naustud.io>
2016-10-08 20:40:58 +0700

    update some copy

--- VNZIP/newnew/assets/js/main.js
+++ VNZIP/newnew/assets/js/main.js
@@ -1,39 +1,51 @@
 $(document).ready(function() {
+    var lang = window.location.pathname.split('/')[1] || 'vi';
+    var successMessage = 'Thông tin của bạn đã được gửi đi. Cảm ơn bạn đã đăng ký. Chúng tôi sẽ liên hệ với bạn trong thời gian sớm nhất.';
+    var failMessage = 'Đã có lỗi xảy ra. Xin lỗi bạn vì sự bất tiện này. Vui lòng thử lại sau';
+    var submittingText = 'ĐANG GỬI...';
+    var submitText = 'ĐĂNG KÝ';
+    if (lang === 'en') {
+        successMessage = 'Your information have been sent. Thank you for registration. We will contact you as soon as possible.';
+        failMessage = 'There are some errors. Sorry for this inconvienent. Please try again later';
+        submittingText = 'SENDING...';
+        submitText = 'SEND';
+    }
+
     $('#contact-form').on('submit', function(e) {
         e.preventDefault();
         var submitButton = $('.form-btn-submit');
         var form = $(this)[0];
         var params = $.param({
-           lastname: form.lastname.value,
-           name: form.name.value,
-           phone: form.phone.value,
-           email: form.email.value,
-           address: form.address.value,
-           province: form.province.value,
-           country: form.country.value,
-           childno: form.childno.value,
-           useragent: navigator.userAgent
+            lastname: form.lastname.value,
+            name: form.name.value,
+            phone: form.phone.value,
+            email: form.email.value,
+            address: form.address.value,
+            province: form.province.value,
+            country: form.country.value,
+            childno: form.childno.value,
+            useragent: navigator.userAgent
         });
 
-        submitButton.html('ĐANG GỬI...');
+        submitButton.html(submittingText);
         submitButton.attr('disabled','');
         $.ajax({
             url: 'https://script.google.com/macros/s/AKfycbySydaat2gvvqufr59LrIcG6Q3Jclbnr42x946cU9iSCmEd9HQ/exec?' + params,
             method: 'GET',
             error: function(jqXHR, textStatus, errorThrow) {
                 if (jqXHR.status === 405 || jqXHR.status === 0) {
-                    alert('Thông tin của bạn đã được gửi đi. Chúng tôi sẽ liên hệ với bạn trong thời gian sớm nhất.');
+                    alert(successMessage);
                 } else {
-                    alert('Đã có lỗi xảy ra. Xin lỗi bạn vì sự bất tiện này. Vui lòng thử lại sau');
+                    alert(failMessage);
                 }
                 submitButton.removeAttr('disabled');
-                submitButton.html('ĐĂNG KÝ');                        
+                submitButton.html(submitText);                        
             }
         }).done(function(res) {
             if (JSON.parse(res).success) {
-                alert('Thông tin của bạn đã được gửi đi. Chúng tôi sẽ liên hệ với bạn trong thời gian sớm nhất.');
+                alert(successMessage);
                 submitButton.removeAttr('disabled');
-                submitButton.html('ĐĂNG KÝ');
+                submitButton.html(submitText);
             }
         });
     });
